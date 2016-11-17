4b22da6b003a832c3c8a4c4259e778dba60b0edc
Igor Silveira <igoramorimsilveira@hotmail.com>
2016-11-17 23:19:40 +0000

    runTrip para guest

--- Trabalho AEDA/Trabalho AEDA/Agency.cpp
+++ Trabalho AEDA/Trabalho AEDA/Agency.cpp
@@ -1400,11 +1400,11 @@ void Agency::showRecTripsGuest(vector<Trip> recTrips, vector<string> stopCodes)
 	menuHeader();
 	cout << "|                     ";  grey(); cout << "TRIPS AVAILABLE";  white(); cout << "                     |" << endl
 		<< "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-		grey(); cout << setw(3) << "ID" << setw(8) << "Driver" << setw(8) << "Origin" << setw(10) << "Destiny" << setw(9) << "Date" << setw(11) << "Start" << setw(7) << "End" << endl;
-		blue(); cout << "-----------------------------------------------------------" << endl; white();
-		for (unsigned int i = 0; i < recTrips.size(); i++) {
-			cout << recTrips.at(i);
-		}
+	grey(); cout << setw(3) << "ID" << setw(8) << "Driver" << setw(8) << "Origin" << setw(10) << "Destiny" << setw(9) << "Date" << setw(11) << "Start" << setw(7) << "End" << endl;
+	blue(); cout << "-----------------------------------------------------------" << endl; white();
+	for (unsigned int i = 0; i < recTrips.size(); i++) {
+		cout << recTrips.at(i);
+	}
 
 	chooseTripGuest(recTrips, stopCodes);
 	return;
@@ -2764,7 +2764,7 @@ void Agency::chooseTripGuest(vector<Trip> recTrips, vector<string> stopCodes)
 
 	Transaction trans(guestID, currentDate, payment);
 	Transactions.push_back(trans);
-	
+
 	yellow(); cout << "\n Success! You were added to the trip!\n"; white();
 	Sleep(2000);
 	cin.clear();
@@ -2891,26 +2891,13 @@ void Agency::runTrip(int tripID) {
 				//se o ID � negativo, procura-se no vetor de Guest
 				if (id < 0)
 				{
-					/*for (size_t j = 0; j < Guests.size(); j++)
+					//se está na proxima paragem continua e atualiza-se o numero de stops do guest
+					if (CHECK == -1)
 					{
-						//encontrado o guest do ID respetivo
-						if (Guests[j]->getID() == id)
-						{
-							//se está na proxima paragem continua e atualiza-se o numero de stops do guest
-							if (CHECK != -1)
-							{
-								Guests[j]->updateNumStops();
-							}
-							else
-							{
-								//se nao esta é porque saiu nesta
-								Transaction trans(id, ActiveTrips[tripIndex].getDate(), (float)Guests[j]->getNumStops()); //TODO: multiplicar por valor
-								Transactions.push_back(trans);
-								usersWhoExitedAfter.push_back(id);
+						//se nao esta é porque saiu nesta
+						usersWhoExitedAfter.push_back(id);
+					}
 
-							}
-						}
-					}*/
 				}
 			}
 
@@ -3018,7 +3005,15 @@ void Agency::runTrip(int tripID) {
 
 				for (size_t i = 0; i < stops.at(stopIt).getPassengers().size(); i++)
 				{
-					cout << Users[getPos(stops.at(stopIt).getPassengers().at(i))]->getName();
+					if (stops.at(stopIt).getPassengers()[i] == -1)
+					{
+						cout << "Guest";
+					}
+					else
+					{
+						cout << Users[getPos(stops.at(stopIt).getPassengers().at(i))]->getName();
+					}
+
 					if (i != stops.at(stopIt).getPassengers().size() - 1)
 					{
 						cout << ", ";
