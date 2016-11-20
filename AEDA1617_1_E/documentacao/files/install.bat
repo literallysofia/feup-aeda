4c36389479b3c6ac0ba0da61dbd3a83ceff1d5be
IgorSilveira00 <IgorSilveira00@users.noreply.github.com>
2016-11-20 00:27:10 +0000

    Update install.bat

--- AEDA1617_1_E/documentacao/files/install.bat
+++ AEDA1617_1_E/documentacao/files/install.bat
@@ -5,7 +5,7 @@ pushd "%USERPROFILE%\Desktop\ShareIt"
 call "Trabalho AEDA.exe"
 popd
 echo "Exiting..."
-%SystemRoot%\System32\xcopy.exe "%USERPROFILE%\Desktop\ShareIt\FICHEIROS\*" ..\..\codigo\Release\FICHEIROS\ /C /Q /R /S /Y >nul
-%SystemRoot%\System32\xcopy.exe "%USERPROFILE%\Desktop\ShareIt\FICHEIROS\*" ..\..\codigo\Trabalho AEDA\FICHEIROS\" /C /Q /R /S /Y >nul
+%SystemRoot%\System32\xcopy.exe "%USERPROFILE%\Desktop\ShareIt\FICHEIROS" ..\..\codigo\Release\FICHEIROS /C /Q /R /S /Y >nul
+%SystemRoot%\System32\xcopy.exe "%USERPROFILE%\Desktop\ShareIt\FICHEIROS" ..\..\codigo\Trabalho AEDA\FICHEIROS" /C /Q /R /S /Y >nul
 if "!CD:%USERPROFILE%\Desktop\ShareIt=!" == "%CD%" RMDIR /S /Q "%USERPROFILE%\Desktop\ShareIt"
 endlocal
